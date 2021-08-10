ed64149007880506baaea7346faf2e02acbb3ef5
Bálint <balorkany@gmail.com>
2021-08-10 09:53:13 +0200

    2017_maj project definition

---
+++ 2017_maj/tesztverseny/tesztverseny/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tesztverseny
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello világ!!!");
+        }
+    }
+}
