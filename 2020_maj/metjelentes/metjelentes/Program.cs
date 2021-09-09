190856cdd5dea809a17e30b29db36dd1c57682b2
Bálint <balorkany@gmail.com>
2021-09-09 19:19:01 +0200

    2020_maj: 2,3,4,6 feladatok, 5 Todo

--- 2020_maj/metjelentes/metjelentes/Program.cs
+++ 2020_maj/metjelentes/metjelentes/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.IO;
+using System.Linq;
 using System.Net;
 
 namespace metjelentes
@@ -16,8 +17,90 @@ namespace metjelentes
             //Feladat 1
             Feladat01();
             
-            Console.WriteLine(taviratok[0].ido);
-            Console.WriteLine(taviratok[0].szelerosseg);
+            //Feladat 2
+            Console.WriteLine("2. Feladat");
+            Feladat02();
+            
+            //Feladat 3
+            Console.WriteLine("3. Feladat");
+            Feladat03();
+       
+            //Feladat 4
+            Console.WriteLine("4. Feladat");
+            Feladat04();
+            
+            //Feladat 5
+            Console.WriteLine("5. Feladat");
+            Feladat05();
+            
+            //Feladat 6
+            Console.WriteLine("6. Feladat");
+            Feladat06();
+        }
+
+        private static void Feladat06()
+        {
+            var grouppedByTelepules = taviratok.GroupBy(t => t.telepules);
+
+            foreach (var group in grouppedByTelepules)
+            {
+                string telepules = group.Key;
+                List<Tavirat> telepulesList = group.ToList();
+                StreamWriter fileKi = new StreamWriter($"{telepules}.txt");
+
+                fileKi.WriteLine(telepules);
+                foreach (var t in telepulesList)
+                {
+                    string szelerossegKettoskeresztek = "".PadLeft(t.szelerosseg, '#');
+                    fileKi.WriteLine($"{t.ido} {szelerossegKettoskeresztek}");
+                }
+                
+                fileKi.Close();
+            }
+            
+            Console.WriteLine("A fájlok elkészültek.");
+        }
+        
+        private static void Feladat05()
+        {
+            Console.WriteLine("ToDo..");
+        }
+
+        private static void Feladat04()
+        {
+            List<Tavirat> szelcsendes = taviratok.Where(t => t.szelerosseg == 0).ToList();
+
+            if (szelcsendes.Count > 0)
+            {
+                foreach (var t in szelcsendes)
+                {
+                    Console.WriteLine($"{t.telepules} {t.ido}");
+                
+                }    
+            }
+            else
+            {
+                Console.WriteLine("Nem volt szélcsend a mérések idején.");
+            }
+        }
+
+        private static void Feladat03()
+        {
+            Tavirat legalacsonyabbHom = taviratok.OrderBy(t => t.homerseklet).First();
+            Tavirat legmagasabbHom = taviratok.OrderBy(t => t.homerseklet).Last();
+            
+            Console.WriteLine($"A legalacsonyabb hőmérséklet: {legalacsonyabbHom.telepules} {legalacsonyabbHom.ido} {legalacsonyabbHom.homerseklet} fok.");
+            Console.WriteLine($"A legmagasabb hőmérséklet: {legmagasabbHom.telepules} {legmagasabbHom.ido} {legmagasabbHom.homerseklet} fok.");
+        }
+
+        private static void Feladat02()
+        {
+            Console.Write("Adja meg a település kódját! Település: ");
+            string telepulesBe = Console.ReadLine();
+
+            string utolsoIdo = taviratok.Where(t => t.telepules == telepulesBe).Last().ido;
+            
+            Console.WriteLine($"Az utolsó mérési adat a megadott telepükésről {utolsoIdo}-kor érkezett.");
         }
 
         private static void Feladat01()
@@ -30,15 +113,11 @@ namespace metjelentes
                 Tavirat tavirat = new Tavirat();
 
                 tavirat.telepules = splittedLine[0];
-                tavirat.ido = $"{splittedLine[1].Substring(0, 2)}:{splittedLine[1].Substring(2,2)}";
+                tavirat.ido = splittedLine[1].Insert(2, ":");
+                //vagy másként: tavirat.ido = $"{splittedLine[1].Substring(0, 2)}:{splittedLine[1].Substring(2,2)}";
                 tavirat.homerseklet = Int32.Parse(splittedLine[3]);
                 tavirat.szelerosseg = Int32.Parse(splittedLine[2].Substring(3,2));
                 
-                // ToDo:
-                // - idő óó:pp formába
-                // - szélerősség int-ként, nem kell szélirány (szélcsend?)
-                // Többi értelem szerűen..
-
                 taviratok.Add(tavirat);
             }
         }
